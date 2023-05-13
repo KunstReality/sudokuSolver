31e3cacc483630d154c2faf2a3f7ce3cf67e6c28
jarviskunstreality@gmail.com <jarviskunstreality@gmail.com>
2023-05-13 11:35:33 +0200

    openCV and main functions updated

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -3,6 +3,39 @@ import math
 import numpy as np
 import cv2
 import imutils
+from matplotlib import pyplot as plt
+
+
+def show_image(img):
+    cv2.imshow('image', img)  # Display the image
+    cv2.waitKey(0)  # Wait for any key to be pressed (with the image window active)
+    cv2.destroyAllWindows()  # Close all windows
+
+
+def plot_many_images(images, titles, rows=1, columns=2):
+    for i, image in enumerate(images):
+        plt.subplot(rows, columns, i + 1)
+        plt.imshow(image, 'gray')
+        plt.title(titles[i])
+        plt.xticks([]), plt.yticks([])  # Hide tick marks
+    plt.show()
+
+
+def display_points(in_img, points, color=(0, 0, 255)):
+    img = in_img.copy()
+    for point in points:
+        img = cv2.drawContours(img, [point], -1, color, 3)
+    show_image(img)
+    return img
+
+
+def display_rects(in_img, contours, color=255):
+    img = in_img.copy()
+    for rect in contours:
+        img = cv2.drawContours(img, [rect], -1, color, 3)
+    show_image(img)
+    return img
+
 
 def locate_sudoku(img):
     # get the gray img
@@ -34,15 +67,28 @@ def locate_sudoku(img):
         if math.fabs(cv2.contourArea(approximated_poly)) > 1000:
             rect = cv2.minAreaRect(approximated_poly)
             box = cv2.boxPoints(rect)
-            box = np.intp(box)
+            box = np.array(box, dtype=np.int32)
             board = box
             break
+    return board
 
+
+# Projekt the board into a 900 * 900 window
+def get_perspektiveProjection(img, board, height=900, width=900):
+    board_vertecies = np.float32([board[0], board[1], board[2], board[3]])
+    box_vertecies = np.float32([[0, 0], [0, height], [width, height], [width, 0]])
+
+    projektMatrix = cv2.getPerspectiveTransform(board_vertecies, box_vertecies)
+    return cv2.warpPerspective(img, projektMatrix, (height, width))
+
+
+def main():
+    img = cv2.imread('sudoku2.jpg')
+    board = locate_sudoku(img)
     if board is not None:
-        test_img = cv2.drawContours(img.copy(), [board], -1, (0, 255, 0), 3)
-        cv2.imshow("TestImage", test_img)
+        display_points(img, board)
+        display_rects(img, [board])
 
 
-img = cv2.imread('sudoku3.jpg')
-locate_sudoku(img)
-cv2.waitKey()
+if __name__ == '__main__':
+    main()
