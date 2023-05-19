82db269970fcba2c343a4fce14c52ff7ac47744e
jarviskunstreality@gmail.com <jarviskunstreality@gmail.com>
2023-05-19 22:33:27 +0200

    cell locator added

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -14,15 +14,13 @@ def display_points(in_img, points, color=(0, 0, 255)):
     img = in_img.copy()
     for point in points:
         img = cv2.drawContours(img, [point], -1, color, 3)
-    show_image(img)
     return img
 
 
-def display_rects(in_img, contours, color=255):
+def display_rect(in_img, contours, color=255):
     img = in_img.copy()
     for rect in contours:
         img = cv2.drawContours(img, [rect], -1, color, 3)
-    show_image(img)
     return img
 
 def display_lines(in_img, lines, color=(0, 0, 255)):
@@ -30,7 +28,6 @@ def display_lines(in_img, lines, color=(0, 0, 255)):
     for line in lines:
         x1, y1, x2, y2 = line[0]
         img = cv2.line(img, (x1, y1), (x2, y2), color, 2, cv2.LINE_AA)
-    show_image(img)
     return img
 
 #Blur img to reduce Noise
@@ -42,36 +39,34 @@ def preprocess_img(img):
     # it is also possible to use a Gaussian Filter here
     # @params: d, sigma color, sigma Space
     gauss_img = cv2.GaussianBlur(img_gray, (9, 9), 0)
-    threshold = cv2.adaptiveThreshold(gauss_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    threshold = cv2.adaptiveThreshold(gauss_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 9, 2)
 
-    kernel = np.array([[0., 1., 0.], [1., 1., 1.], [0., 1., 0.]], dtype=np.uint8)
-    erosion_img = cv2.erode(threshold, kernel)
-    return erosion_img
+    return threshold
 
 def locate_sudoku(img):
     # find edges using Canny
     # @params: first and second Threshold
-    edges = cv2.Canny(img, 50, 150)
+    edges = cv2.Canny(img, 60, 150)
     # find the vertices of two contour.
     vertices = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(vertices)
 
     # sort contours based on the Area of a contour from big to small
-    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 
     # find the actual board in the img
     board = None
     for contour in sorted_contours:
-        approximated_poly = cv2.approxPolyDP(contour, 10, True)
-        if len(approximated_poly) == 4:
-            #board = approximated_poly
-            rect = cv2.minAreaRect(approximated_poly)
-            box = cv2.boxPoints(rect)
-            boards_list = []
-            for i in range(len(box)):
-                boards_list.append(np.array([box[i]], dtype=np.int32))
-            board = np.array(boards_list, dtype=np.int32)
-            break
+        approximated_poly = cv2.approxPolyDP(contour, 100, True)
+        if len(approximated_poly) >= 4:
+            if math.fabs(cv2.contourArea(approximated_poly)) > 1000:
+                rect = cv2.minAreaRect(approximated_poly)
+                box = cv2.boxPoints(rect)
+                point_list = []
+                for i in range(len(box)):
+                    point_list.append(np.array([box[i]], dtype=np.int32))
+                board = np.array(point_list, dtype=np.int32)
+                break
     return board
 
 def sort_points(board):
@@ -102,25 +97,56 @@ def get_perspektiveProjection(img, board, height=900, width=900):
     return cv2.warpPerspective(img, projektMatrix, (height, width))
 
 def find_lines(img):
-    gauss_img = cv2.GaussianBlur(img, (3, 3), 0)
-    edges = cv2.Canny(gauss_img, 60, 150)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 51, 9)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=4)
+
+    edges = cv2.Canny(opening, 100, 150)
     show_image(edges)
     minLineLength = 100
     lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=100, minLineLength=minLineLength, maxLineGap=80)
     return lines
 
+def locate_cells(img):
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 9)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=4)
+    # find the vertices of two contour.
+    edges = cv2.Canny(opening, 100, 150)
+    vertices = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(vertices)
+
+    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=False)
+    # find the actual board in the img
+    cells = []
+    for contour in sorted_contours:
+        peri = cv2.arcLength(contour, True)
+        approximated_poly = cv2.approxPolyDP(contour, 0.015 * peri, True)
+        if len(approximated_poly) >= 3:
+            if 2000 < math.fabs(cv2.contourArea(approximated_poly)) < 10000:
+                x, y, w, h = cv2.boundingRect(approximated_poly)
+                cells.append(np.array([x, y, x+w, y+h], dtype=np.int32))
+
+    return np.array(cells, dtype=np.int32)
 
 def main():
     for i in range(1, 9):
         img = cv2.imread('sudoku' + str(i) + '.jpg')
+        print(i)
         processed_img = preprocess_img(img)
         board = locate_sudoku(processed_img)
         if board is not None:
             perspective_img = get_perspektiveProjection(img, board)
-            lines = find_lines(perspective_img)
-            if lines is not None:
-                display_lines(perspective_img, lines)
-
+            cells = locate_cells(perspective_img)
+            for cell in cells:
+                cv2.rectangle(perspective_img, (cell[0], cell[1]), (cell[2], cell[3]), (36, 255, 12), 3)
+            show_image(perspective_img)
 
 if __name__ == '__main__':
     main()
